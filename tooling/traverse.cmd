f8f4d7d003ca2d63cde2cee9f5bbe978104cf1e0
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-01-29 00:47:33 +0400

    fix(tooling): wrong reference to powershell counterpart

--- tooling/traverse.cmd
+++ tooling/traverse.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& {.\traverse.ps1 %*}; exit $LASTEXITCODE"
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& {%~dpn0.ps1 %*}; exit $LASTEXITCODE"
