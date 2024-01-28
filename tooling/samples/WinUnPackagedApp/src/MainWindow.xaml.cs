2af0f324008ac97936a021bf8b9efa8b3bf1c899
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-01-29 00:46:19 +0400

    chore(tooling): add WinUI un-packaged app sample

---
+++ tooling/samples/WinUnPackagedApp/src/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+/* ReSharper disable PrivateFieldCanBeConvertedToLocalVariable */
+
+namespace DroidNet.Samples.WinPackagedApp;
+
+/// <summary>The User Interface's main window.</summary>
+public sealed partial class MainWindow
+{
+    public MainWindow() => this.InitializeComponent();
+}
