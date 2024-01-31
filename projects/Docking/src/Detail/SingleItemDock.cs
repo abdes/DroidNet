e423173c273ab93afc6f07663366f66ce57e859d
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-01-31 15:58:04 +0400

    feat(docking): add a single item dock type

---
+++ projects/Docking/src/Detail/SingleItemDock.cs
@@ -0,0 +1,18 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+namespace DroidNet.Docking.Detail;
+
+/// <summary>
+/// Represents a dock that holds only one dockable. Any attempt to add a
+/// <see cref="IDockable" /> to it will result in replacing the current one.
+/// </summary>
+public class SingleItemDock : Dock
+{
+    protected SingleItemDock()
+    {
+    }
+
+    public static SingleItemDock? New() => Factory.CreateDock(typeof(SingleItemDock)) as SingleItemDock;
+}
