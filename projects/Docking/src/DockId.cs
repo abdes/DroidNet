0650e6818c8557cdd09a9d1dfefd5c2a4a628ad9
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-02-01 19:04:59 +0400

    refactor(docking): override equality for DockId type

--- projects/Docking/src/DockId.cs
+++ projects/Docking/src/DockId.cs
@@ -8,5 +8,9 @@ public readonly struct DockId(int value)
 {
     public int Value { get; } = value;
 
+    public bool Equals(DockId other) => this.Value == other.Value;
+
+    public override int GetHashCode() => this.Value;
+
     public override string ToString() => $"{this.Value}";
 }
