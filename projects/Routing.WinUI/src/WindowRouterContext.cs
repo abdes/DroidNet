31881af1bf4f9c3bb92caed216ccf386531fd103
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-02-20 13:48:56 +0400

    fix(routing): avoid an exception when getting the updated window title

--- projects/Routing.WinUI/src/WindowRouterContext.cs
+++ projects/Routing.WinUI/src/WindowRouterContext.cs
@@ -10,16 +10,29 @@ using Microsoft.UI.Xaml;
 /// An extended <see cref="RouterContext" />, which keeps track of the
 /// <see cref="Window" /> associated with it.
 /// </summary>
-/// <param name="target">The context's target name.</param>
-/// <param name="window">
-/// The <see cref="Window" /> associated with this context.
-/// </param>
-internal sealed class WindowRouterContext(string target, Window window) : RouterContext(target)
+internal sealed class WindowRouterContext : RouterContext
 {
     // Used for debugging. We want to keep track of the last known window title
     // because when the window is closed, we cannot get its Title property
     // anymore.
-    private string windowTitle = window.Title;
+    private string windowTitle;
+    private bool windowClosed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowRouterContext" />
+    /// class.
+    /// </summary>
+    /// <param name="target">The context's target name.</param>
+    /// <param name="window">
+    /// The <see cref="Window" /> associated with this context.
+    /// </param>
+    public WindowRouterContext(string target, Window window)
+        : base(target)
+    {
+        this.windowTitle = window.Title;
+        this.Window = window;
+        this.Window.Closed += (_, _) => this.windowClosed = true;
+    }
 
     /// <summary>
     /// Gets the <see cref="Window" /> associated with this context.
@@ -27,18 +40,14 @@ internal sealed class WindowRouterContext(string target, Window window) : Router
     /// <value>
     /// The <see cref="Window" /> associated with this context.
     /// </value>
-    internal Window Window { get; } = window;
+    internal Window Window { get; }
 
     /// <inheritdoc />
     public override string ToString()
     {
-        try
+        if (!this.windowClosed)
         {
             this.windowTitle = this.Window.Title;
-        }
-        catch
-        {
-            // ignored, we'll use the last known value of the window's title
         }
 
         return $"{this.Target}:{this.Window.GetType().Name}('{this.windowTitle}')";
