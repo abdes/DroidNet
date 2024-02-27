a7b59f71ab6f44cb2a9a2b2c1b09a9fa547773b7
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-02-27 20:38:26 +0400

    refactor(docking): simplify the code

--- projects/Docking/src/Detail/DockableFactory.cs
+++ projects/Docking/src/Detail/DockableFactory.cs
@@ -20,7 +20,7 @@ public partial class Dockable
     public static Dockable? FromId(string id)
         => Factory.TryGetDockable(id, out var dockable) ? dockable : null;
 
-    public static Dockable? New(string id) => Factory.CreateDockable(id);
+    public static Dockable New(string id) => Factory.CreateDockable(id);
 
     public void Dispose()
     {
@@ -35,29 +35,21 @@ public partial class Dockable
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
     }
 
-    private static class Factory
+    internal static class Factory
     {
         private static readonly ConcurrentDictionary<string, Dockable> Dockables = [];
 
-        public static Dockable? CreateDockable(string id)
+        public static Dockable CreateDockable(string id)
         {
             if (Dockables.ContainsKey(id))
             {
                 throw new InvalidOperationException($"attempt to create a dockable with an already used ID: {id}");
             }
 
-            try
-            {
-                var dockable = new Dockable(id);
-                var added = Dockables.TryAdd(id, dockable);
-                Debug.Assert(added, "adding a dockable with a new id should always succeed");
-                return dockable;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Failed to create a new dockable: {ex}");
-                return null;
-            }
+            var dockable = new Dockable(id);
+            var added = Dockables.TryAdd(id, dockable);
+            Debug.Assert(added, "adding a dockable with a new id should always succeed");
+            return dockable;
         }
 
         public static bool TryGetDockable(string id, out Dockable? dockable)
@@ -75,20 +67,7 @@ public partial class Dockable
         {
             private int position = -1;
 
-            public IDockable Current
-            {
-                get
-                {
-                    try
-                    {
-                        return Dockables.Values.ElementAt(this.position);
-                    }
-                    catch (IndexOutOfRangeException)
-                    {
-                        throw new InvalidOperationException();
-                    }
-                }
-            }
+            public IDockable Current => Dockables.Values.ElementAt(this.position);
 
             object IEnumerator.Current => this.Current;
 
