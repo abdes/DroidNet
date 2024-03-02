bcaf25686b473a889f39888b072f9d068b70d8ca
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-03-02 20:36:35 +0400

    test: add unit test cases

---
+++ projects/Docking/tests/Detail/LengthTests.cs
@@ -0,0 +1,24 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+namespace DroidNet.Docking.Detail;
+
+using FluentAssertions;
+
+[TestClass]
+public class LengthTests
+{
+    [TestMethod]
+    public void Create_WithInvalidString_Throws()
+    {
+        // Arrange
+        const string value = "xyz";
+
+        // Act
+        var act = () => new Width(value);
+
+        // Assert
+        _ = act.Should().Throw<ArgumentException>();
+    }
+}
