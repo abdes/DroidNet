6ef851f1e159b3801e96673abc4c2b2e8febc730
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-03-02 11:39:11 +0400

    test: add unit tests

---
+++ projects/Docking/tests/ToolDockTests.cs
@@ -0,0 +1,22 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+namespace DroidNet.Docking;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ToolDockTests
+{
+    [TestMethod]
+    public void CanCreate()
+    {
+        // Arrange / Act
+        using var dock = ToolDock.New();
+
+        // Assert
+        _ = dock.Should().NotBeNull();
+    }
+}
