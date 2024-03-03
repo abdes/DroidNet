cafb712f0bbc9c1a7380054d1f32fb71eaddc017
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-03-03 12:01:10 +0400

    test: add docks with no-close or no-minimize

--- projects/Docking/tests/Mocks/SimpleDock.cs
+++ projects/Docking/tests/Mocks/SimpleDock.cs
@@ -9,3 +9,20 @@ using DroidNet.Docking.Detail;
 
 [ExcludeFromCodeCoverage]
 internal sealed class SimpleDock : Dock;
+
+internal sealed class NoMinimizeDock : Dock
+{
+    public override bool CanMinimize => false;
+}
+
+internal sealed class NoCloseDock : Dock
+{
+    public override bool CanClose => false;
+}
+
+internal sealed class SimpleCenterDock : Dock
+{
+    public override bool CanMinimize => false;
+
+    public override bool CanClose => false;
+}
