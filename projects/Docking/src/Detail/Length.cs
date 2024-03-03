ae57dfb896e06423c5ec22f2f067fe47805784bb
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-03-03 11:53:58 +0400

    refactor: override equality and hash

--- projects/Docking/src/Detail/Length.cs
+++ projects/Docking/src/Detail/Length.cs
@@ -8,36 +8,37 @@ using System.Globalization;
 using System.Text.RegularExpressions;
 
 /// <summary>
-/// Encapsulates a length value as a <see langword="string"/> compatible with <see
-/// href="https://learn.microsoft.com/en-us/dotnet/api/system.windows.gridlength?view=windowsdesktop-8.0">
+/// Encapsulates a length value as a <see langword="string" /> compatible with <see href="https://learn.microsoft.com/en-us/dotnet/api/system.windows.gridlength?view=windowsdesktop-8.0">
 /// GridLength</see> format.
 /// </summary>
 /// <remarks>
 /// This is an abstract base class, that should be specialized to represent specific dimensions such as width or height, while
 /// avoiding the errors causes by multiple dimensions with the same type appearing as arguments to methods.
 /// </remarks>
-/// <seealso cref="Width"/>
-/// <seealso cref="Height"/>
+/// <seealso cref="Width" />
+/// <seealso cref="Height" />
 public abstract partial class Length
 {
     private readonly string? value;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Length"/> class with a string value.
+    /// Initializes a new instance of the <see cref="Length" /> class with a string value.
     /// </summary>
-    /// <param name="value">The value as a string, which can be null. A <see langword="null"/> value indicates the absence of
-    /// any requirement regarding this dimension. When not <see langword="null"/>, the value must be of one of the following
+    /// <param name="value">The value as a string, which can be null. A <see langword="null" /> value indicates the absence of
+    /// any requirement regarding this dimension. When not <see langword="null" />, the value must be of one of the following
     /// formats:
     /// <list type="bullet">
-    /// <item><term>auto</term>
+    /// <item>
+    /// <term>auto</term>
     /// <description>value should be determined automatically.</description>
     /// </item>
-    /// <item><term>numeric</term>
-    /// <description>an explicit value as a <see langword="double"/>.</description>
+    /// <item>
+    /// <term>numeric</term>
+    /// <description>an explicit value as a <see langword="double" />.</description>
     /// </item>
-    /// <item><term>star</term>
-    /// <description>the value is expressed as a weighted proportion of the available space, specified as a <see
-    /// langword="double"/> followed with the character '<c>*</c>'. A lone star is equivalent to "1*".</description>
+    /// <item>
+    /// <term>star</term>
+    /// <description>the value is expressed as a weighted proportion of the available space, specified as a <see langword="double" /> followed with the character '<c>*</c>'. A lone star is equivalent to "1*".</description>
     /// </item>
     /// </list>
     /// </param>
@@ -53,7 +54,7 @@ public abstract partial class Length
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Length"/> class with a specific numeric value as pixels.
+    /// Initializes a new instance of the <see cref="Length" /> class with a specific numeric value as pixels.
     /// </summary>
     /// <param name="pixels">
     /// The number of device-independent pixels (96 pixels-per-inch), rounded to the nearest integer.
@@ -61,18 +62,34 @@ public abstract partial class Length
     protected Length(double pixels) => this.value = double.Round(pixels).ToString(CultureInfo.InvariantCulture);
 
     /// <summary>
-    /// Gets a value indicating whether the underlying value in this <see cref="Length"/> is <see langword="null"/> or is an empty
+    /// Gets a value indicating whether the underlying value in this <see cref="Length" /> is <see langword="null" /> or is an empty
     /// string.
     /// </summary>
     public bool IsNullOrEmpty => string.IsNullOrEmpty(this.value);
 
     /// <summary>
-    /// Implicitly convert the underlying value of a <see cref="Length"/> to a string.
+    /// Implicitly convert the underlying value of a <see cref="Length" /> to a string.
     /// </summary>
     /// <param name="length">the length to be converted to a string.</param>
     public static implicit operator string?(Length? length) => length?.value;
 
+    /// <inheritdoc />
     public override string ToString() => this.value ?? "?";
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || this.GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var other = (Length)obj;
+        return this.value == other.value;
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode() => this.value != null ? this.value.GetHashCode() : 0;
 
     [GeneratedRegex(@"^(auto|(\d+(\.\d+)?)?\*?)$")]
     private static partial Regex GridLengthRegEx();
