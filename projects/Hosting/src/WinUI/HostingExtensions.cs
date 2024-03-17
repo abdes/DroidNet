99b2c0680121bc41ec60ad9bfc397395300935bb
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-03-18 02:14:47 +0400

    refactor: enclose all the initialization code inside the ConfigureServices call

--- projects/Hosting/src/WinUI/HostingExtensions.cs
+++ projects/Hosting/src/WinUI/HostingExtensions.cs
@@ -10,76 +10,58 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
 
 /// <summary>
-/// Contains helper extensions for <see cref="HostApplicationBuilder" /> to
-/// configure the WinUI service hosting.
+/// Contains helper extensions for <see cref="HostApplicationBuilder" /> to configure the WinUI service hosting.
 /// </summary>
 public static class HostingExtensions
 {
     /// <summary>
-    /// The key used to access the <see cref="HostingContext" /> instance in
-    /// <see cref="IHostBuilder.Properties" />.
+    /// The key used to access the <see cref="HostingContext" /> instance in <see cref="IHostBuilder.Properties" />.
     /// </summary>
     public const string HostingContextKey = "UserInterfaceHostingContext";
 
-    /// <summary>
-    /// Configures the host builder for a Windows UI (WinUI) application.
-    /// </summary>
-    /// <typeparam name="TApplication">
-    /// The concrete type for the <see cref="Application" /> class.
-    /// </typeparam>
+    /// <summary>Configures the host builder for a Windows UI (WinUI) application.</summary>
+    /// <typeparam name="TApplication">The concrete type for the <see cref="Application" /> class.</typeparam>
+    /// <param name="builder">The host builder to which the WinUI service needs to be added.</param>
+    /// <returns>The host builder for chaining calls.</returns>
+    /// <exception cref="ArgumentException">When the application's type does not extend <see cref="Application" />.</exception>
     /// <remarks>
     /// <para>
-    /// This method configures the host builder to support a Windows UI (WinUI)
-    /// application. It sets up the necessary services, including the hosting
-    /// context, user interface thread, and the hosted service for the user
-    /// interface.
+    /// This method configures the host builder to support a Windows UI (WinUI) application. It sets up the necessary services,
+    /// including the hosting context, user interface thread, and the hosted service for the user interface.
     /// </para>
     /// <para>
-    /// It attempts to find a <see cref="HostingContext" /> instance from the
-    /// host builder properties and if not available creates one and adds it as
-    /// a singleton service and as a <see cref="BaseHostingContext" /> service
-    /// for use by the <see cref="UserInterfaceHostedService" />.
+    /// It attempts to find a <see cref="HostingContext" /> instance from the host builder properties and if not available creates
+    /// one and adds it as a singleton service and as a <see cref="BaseHostingContext" /> service for use by the <see cref="UserInterfaceHostedService" />.
     /// </para>
     /// <para>
-    /// Upon successful completion, the dependency injector will be able to
-    /// provide the single instance of the application as
-    /// <typeparamref name="TApplication" /> and as <see cref="Application" />
-    /// if it is not the same type.
+    /// Upon successful completion, the dependency injector will be able to provide the single instance of the application as
+    /// <typeparamref name="TApplication" /> and as <see cref="Application" /> if it is not the same type.
     /// </para>
     /// </remarks>
-    /// <param name="hostBuilder">
-    /// The host builder to which the WinUI service needs to be added.
-    /// </param>
-    /// <returns>The host builder for chaining calls.</returns>
-    /// <exception cref="ArgumentException">
-    /// When the application's type does not extend <see cref="Application" />.
-    /// </exception>
-    public static IHostBuilder ConfigureWinUI<TApplication>(this IHostBuilder hostBuilder)
-        where TApplication : Application
-    {
-        HostingContext context;
-        if (hostBuilder.Properties.TryGetValue(HostingContextKey, out var contextAsObject))
+    public static IHostBuilder ConfigureWinUI<TApplication>(this IHostBuilder builder)
+        where TApplication : Application => builder.ConfigureServices(
+        (builderContext, services) =>
         {
-            context = (HostingContext)contextAsObject;
-        }
-        else
-        {
-            context = new HostingContext { IsLifetimeLinked = true };
-            hostBuilder.Properties[HostingContextKey] = context;
-        }
+            HostingContext hostingContext;
+            if (builderContext.Properties.TryGetValue(HostingContextKey, out var hostingContextAsObject))
+            {
+                hostingContext = (HostingContext)hostingContextAsObject;
+            }
+            else
+            {
+                hostingContext = new HostingContext { IsLifetimeLinked = true };
+                builder.Properties[HostingContextKey] = hostingContext;
+            }
 
-        _ = hostBuilder.ConfigureServices(
-            services => services.AddSingleton(context)
+            _ = services
+                .AddSingleton(hostingContext)
                 .AddSingleton<IUserInterfaceThread, UserInterfaceThread>()
                 .AddHostedService<UserInterfaceHostedService>()
-                .AddSingleton<TApplication>());
+                .AddSingleton<TApplication>();
 
-        if (typeof(TApplication) != typeof(Application))
-        {
-            _ = hostBuilder.ConfigureServices(
-                services => services.AddSingleton<Application>(sp => sp.GetRequiredService<TApplication>()));
-        }
-
-        return hostBuilder;
-    }
+            if (typeof(TApplication) != typeof(Application))
+            {
+                _ = services.AddSingleton<Application>(sp => sp.GetRequiredService<TApplication>());
+            }
+        });
 }
