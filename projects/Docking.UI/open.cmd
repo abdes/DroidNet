87d4864d77f53babf05d635f26c4fa4551cab3fc
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-04-02 12:40:24 +0400

    refactor: merge Docking.UI into Docking project

--- projects/Docking.UI/open.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-SET COMMAND=dotnet slngen -d . -o AllDocking.sln --folders false .\**\*.csproj
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& {%COMMAND% %*}; exit $LASTEXITCODE"
