9881ef0dfb55ad34142f75bada6ea8def5aa9d59
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-11-04 15:41:49 +0400

    refactor: code style and improve documentation

--- projects/Routing.Abstractions/src/Parameters.cs
+++ projects/Routing.Abstractions/src/Parameters.cs
@@ -7,16 +7,17 @@ namespace DroidNet.Routing;
 using System.Collections;
 
 /// <summary>
-/// Represents a collection of <see cref="Parameter">URL parameters</see>.
+/// Represents a collection of URL parameters (<see cref="Parameter" />).
 /// </summary>
 public class Parameters : IParameters
 {
-    /// <summary>Parameter names are case-insensitive.</summary>
+    /// <summary>Dictionary to store parameters, with case-insensitive names.</summary>
     private readonly Dictionary<string, string?> parameters = new(StringComparer.OrdinalIgnoreCase);
 
     /// <summary>Gets the number of parameters in the collection.</summary>
     public int Count => this.parameters.Count;
 
+    /// <summary>Gets a value indicating whether the collection is empty.</summary>
     public bool IsEmpty => this.parameters.Count == 0;
 
     /// <inheritdoc />
@@ -27,18 +28,11 @@ public class Parameters : IParameters
 
     /// <summary>
     /// Adds or updates a parameter with the specified name and value.
+    /// If the parameter exists, its value is updated; otherwise, a new parameter is added.
     /// </summary>
-    /// <param name="name">The name of the parameter to add.</param>
-    /// <param name="value">The value of the parameter to add.</param>
-    /// <exception cref="ArgumentException">If the specified parameter name is
-    /// the empty string.</exception>
-    /// <remarks>
-    /// As long as the parameter name is valid, this method is always
-    /// successful. If a parameter with the same name does not exist in the
-    /// collection, a new one with the specified name and value is created and
-    /// added. Otherwise, the existing parameter value is updated with the
-    /// specified value.
-    /// </remarks>
+    /// <param name="name">The name of the parameter.</param>
+    /// <param name="value">The value of the parameter.</param>
+    /// <exception cref="ArgumentException">Thrown if the parameter name is empty.</exception>
     public void AddOrUpdate(string name, string? value)
     {
         if (name.Length == 0)
@@ -52,11 +46,11 @@ public class Parameters : IParameters
     /// <summary>
     /// Adds a parameter with the specified name and value to the collection.
     /// </summary>
-    /// <param name="name">The name of the parameter to add.</param>
-    /// <param name="value">The value of the parameter to add.</param>
-    /// <exception cref="ArgumentException">If the specified parameter name is
-    /// the empty string, or if a parameter with the same name exists in the
-    /// collection.</exception>
+    /// <param name="name">The name of the parameter.</param>
+    /// <param name="value">The value of the parameter.</param>
+    /// <exception cref="ArgumentException">
+    /// Thrown if the parameter name is empty, or if a parameter with the same name already exists in the collection.
+    /// </exception>
     public void Add(string name, string? value)
     {
         if (name.Length == 0)
@@ -68,20 +62,15 @@ public class Parameters : IParameters
     }
 
     /// <summary>
-    /// Attempts the add a parameter with the specified name and value to the
-    /// collection.
+    /// Attempts to add a parameter with the specified name and value to the collection.
     /// </summary>
-    /// <param name="name">The name of the parameter to add.</param>
-    /// <param name="value">The value of the parameter to add.</param>
-    /// <returns><see langword="true" /> if the parameter was added
-    /// successfully; <see langword="false" /> otherwise.</returns>
-    /// <exception cref="ArgumentException">If the specified parameter name is
-    /// the empty string.</exception>
+    /// <param name="name">The name of the parameter.</param>
+    /// <param name="value">The value of the parameter.</param>
+    /// <returns><see langword="true" /> if the parameter was added successfully; otherwise, <see langword="false" />.</returns>
+    /// <exception cref="ArgumentException">Thrown if the parameter name is empty.</exception>
     /// <remarks>
-    /// This method doesn't throw an exception if a parameter with the same name
-    /// exists in the collection. Unlike the <see cref="AddOrUpdate" />, TryAdd
-    /// doesn't override an existing parameter. If a parameter with the same
-    /// name exists, TryAdd does nothing and returns <see langword="false" />.
+    /// This method does not throw an exception if a parameter with the same name exists in the collection. Instead,
+    /// it returns <see langword="false" />.
     /// </remarks>
     public bool TryAdd(string name, string? value)
     {
@@ -97,27 +86,25 @@ public class Parameters : IParameters
     /// Removes the parameter with the specified name from the collection.
     /// </summary>
     /// <param name="name">The name of the parameter to remove.</param>
-    /// <returns><see langword="true" /> if the parameter is successfully found
-    /// and removed; otherwise, <see langword="false" />. This method returns
-    /// <see langword="false" /> if parameter name is not found in the
-    /// collection.</returns>
+    /// <returns>
+    /// <see langword="true" /> if the parameter is successfully removed; otherwise, <see langword="false" />.
+    /// </returns>
     public bool Remove(string name) => this.parameters.Remove(name);
 
     /// <summary>
-    /// Removes all items from the collection and resets the <see cref="Count" /> property to <c>0</c>.
+    /// Clears all parameters from the collection, resetting the <see cref="Count" /> property to zero.
     /// </summary>
     public void Clear() => this.parameters.Clear();
 
+    /// <summary>
+    /// Returns a read-only version of the parameters collection.
+    /// </summary>
+    /// <returns>A <see cref="ReadOnlyParameters">read-only</see> version of the parameters collection.</returns>
     public ReadOnlyParameters AsReadOnly() => new(this);
 
     /// <inheritdoc />
     public IEnumerator<Parameter> GetEnumerator()
-    {
-        foreach (var item in this.parameters)
-        {
-            yield return new Parameter(item.Key, item.Value);
-        }
-    }
+        => this.parameters.Select(item => new Parameter(item.Key, item.Value)).GetEnumerator();
 
     /// <inheritdoc />
     IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
--- projects/Routing.Abstractions/src/ReadOnlyParameters.cs
+++ projects/Routing.Abstractions/src/ReadOnlyParameters.cs
@@ -7,41 +7,43 @@ namespace DroidNet.Routing;
 using System.Collections;
 
 /// <summary>
-/// Represents a read-only collection of <see cref="Parameter">URL
-/// parameters</see>.
+/// Represents a read-only collection of URL parameters (<see cref="Parameter" />).
 /// </summary>
 public class ReadOnlyParameters : IParameters
 {
+    /// <summary>
+    /// The underlying parameters collection.
+    /// </summary>
     private readonly Parameters parameters;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ReadOnlyParameters" />
-    /// class that is a wrapper around the specified <see cref="Parameters" />
-    /// object.
+    /// Initializes a new instance of the <see cref="ReadOnlyParameters" /> class as a wrapper around the specified
+    /// <see cref="Parameters" /> object.
     /// </summary>
     /// <remarks>
-    /// This constructor is internal only because we want ReadOnlyParameters to
-    /// be created only through the <see cref="Parameters.AsReadOnly" /> method.
-    /// <para>
-    /// We do not use the interface type here. We use the concrete class because
-    /// the interface would allow a ReadOnlyParameters object to wrap anything
-    /// that implements the interface, thus allowing, for example, to wrap
-    /// another ReadOnlyParameters, which is not very helpful and should be
-    /// avoided.
-    /// </para>
+    /// This constructor is internal to ensure that <see cref="ReadOnlyParameters" /> objects are created only through
+    /// the <see cref="Parameters.AsReadOnly" /> method. The concrete class <see cref="Parameters" /> is used instead
+    /// of the interface to prevent wrapping another <see cref="ReadOnlyParameters" /> instance.
     /// </remarks>
-    /// <param name="parameters">The parameters to wrap.</param>
+    /// <param name="parameters">The parameters collection to wrap.</param>
     internal ReadOnlyParameters(Parameters parameters) => this.parameters = parameters;
 
-    /// <summary>Gets an empty <see cref="ReadOnlyParameters" />.</summary>
+    /// <summary>
+    /// Gets an empty instance of <see cref="ReadOnlyParameters" />.
+    /// </summary>
     /// <remarks>
     /// The returned instance is immutable and will always be empty.
     /// </remarks>
     public static ReadOnlyParameters Empty { get; } = new([]);
 
-    /// <summary>Gets the number of parameters in the collection.</summary>
+    /// <summary>
+    /// Gets the number of parameters in the collection.
+    /// </summary>
     public int Count => this.parameters.Count;
 
+    /// <summary>
+    /// Gets a value indicating whether the collection is empty.
+    /// </summary>
     public bool IsEmpty => this.parameters.IsEmpty;
 
     /// <inheritdoc />
@@ -51,13 +53,7 @@ public class ReadOnlyParameters : IParameters
     public bool TryGetValue(string name, out string? value) => this.parameters.TryGetValue(name, out value);
 
     /// <inheritdoc />
-    public IEnumerator<Parameter> GetEnumerator()
-    {
-        foreach (var param in this.parameters)
-        {
-            yield return param;
-        }
-    }
+    public IEnumerator<Parameter> GetEnumerator() => this.parameters.GetEnumerator();
 
     /// <inheritdoc />
     IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
