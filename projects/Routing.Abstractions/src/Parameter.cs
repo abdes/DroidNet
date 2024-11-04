a54940020bde9252afac7e22293baeb2a0ea851a
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-11-04 15:24:41 +0400

    refactor: improve documentation

--- projects/Routing.Abstractions/src/Parameter.cs
+++ projects/Routing.Abstractions/src/Parameter.cs
@@ -5,19 +5,20 @@
 namespace DroidNet.Routing;
 
 /// <summary>
-/// Represents a URL segment matrix parameter or a URL query parameter, which
-/// have a name and an optional value. The name and the value (when present) are
-/// completely opaque strings and do not carry any semantic value inside this
-/// implementation.
+/// Represents a URL segment matrix parameter or a URL query parameter, which have a name and an optional value. The name and
+/// value are treated as opaque strings without any inherent meaning in this implementation.
 /// </summary>
 /// <param name="Name">The parameter name.</param>
 /// <param name="Value">The parameter value.</param>
 public readonly record struct Parameter(string Name, string? Value)
 {
-    /// <summary>Produces a string representation of this parameter.</summary>
-    /// <returns>The parameter name followed by its value, separated by the
-    /// equal (<see langword="=" />) sign, unless the parameter has no value. In
-    /// such case, only the parameter name.</returns>
+    /// <summary>
+    /// Returns a string representation of the parameter.
+    /// </summary>
+    /// <returns>
+    /// The parameter name followed by its value, separated by an equal sign ("="), unless the value is <see langword="null" />,
+    /// in which case only the name is returned.
+    /// </returns>
     public override string ToString() => Uri.EscapeDataString(this.Name) +
                                          (this.Value is not null
                                              ? '=' + Uri.EscapeDataString(this.Value)
