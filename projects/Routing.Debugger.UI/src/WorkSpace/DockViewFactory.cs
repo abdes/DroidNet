bd173c971c654a5971331737a2a8a964e41122d1
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-11-08 12:42:16 +0400

    fix: border outside of dock panel causes resize of dock panel

--- projects/Routing.Debugger.UI/src/WorkSpace/DockViewFactory.cs
+++ projects/Routing.Debugger.UI/src/WorkSpace/DockViewFactory.cs
@@ -10,10 +10,7 @@ using DroidNet.Docking.Controls;
 using DroidNet.Docking.Layouts;
 using DroidNet.Routing.Debugger.UI.Docks;
 using DryIoc;
-using Microsoft.UI;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
 
 public class DockViewFactory(IResolver container) : IDockViewFactory
 {
@@ -22,13 +19,8 @@ public class DockViewFactory(IResolver container) : IDockViewFactory
         {
             ViewModel = container.Resolve<Func<object?, EmbeddedAppViewModel>>()(appDock.ApplicationViewModel),
         }
-        : new Border()
+        : new DockPanel()
         {
-            BorderThickness = new Thickness(0.5),
-            BorderBrush = new SolidColorBrush(Colors.Bisque),
-            Child = new DockPanel()
-            {
-                ViewModel = container.Resolve<Func<IDock, DockPanelViewModel>>()(dock),
-            },
+            ViewModel = container.Resolve<Func<IDock, DockPanelViewModel>>()(dock),
         };
 }
