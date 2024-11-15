57d2d5d8193f7e7f3d196201a1b55cf1421d9463
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-11-15 16:54:36 +0400

    test: setup common test env using TestHelpers

---
+++ projects/Hosting/tests/TestEnv.cs
@@ -0,0 +1,31 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+namespace DroidNet.Hosting.Tests;
+
+using System.Diagnostics.CodeAnalysis;
+using DroidNet.TestHelpers;
+using Serilog;
+
+[TestClass]
+[TestCategory("Test Environment Sanity")]
+[ExcludeFromCodeCoverage]
+public class TestEnv : CommonTestEnv
+{
+    [AssemblyInitialize]
+    public static void Init(TestContext context)
+    {
+        _ = context; // unused
+
+        ConfigureLogging(TestContainer);
+        Log.Information("Test session started");
+    }
+
+    [AssemblyCleanup]
+    public static void Close()
+    {
+        Log.Information("Test session ended");
+        Log.CloseAndFlush();
+    }
+}
