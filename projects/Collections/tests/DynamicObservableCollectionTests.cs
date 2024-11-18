25e5a7c3ddcf17d2c11eaa9f8a5baaab862371ac
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2024-11-18 12:58:17 +0400

    test(collection): add unit test cases for coverage

--- projects/Collections/tests/DynamicObservableCollectionTests.cs
+++ projects/Collections/tests/DynamicObservableCollectionTests.cs
@@ -2,24 +2,23 @@
 // at https://opensource.org/licenses/MIT.
 // SPDX-License-Identifier: MIT
 
-namespace DroidNet.Collections.Tests;
-
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using DroidNet.TestHelpers;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DroidNet.Collections.Tests;
 
 [TestClass]
 [ExcludeFromCodeCoverage]
+[TestCategory("Dynamic Observable Collection")]
 public class DynamicObservableCollectionTests
 {
     private readonly Func<int, string> transform = i => $"Item {i.ToString(CultureInfo.InvariantCulture)}";
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Initialize")]
-    public void DynamicObservableCollection_InitializesWithTransformedItems()
+    public void Initialize_WhenSourceHasItems_TransformsItems()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -38,8 +37,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_AddsTransformedItemsOnSourceAdd()
+    public void Add_WhenSourceAddsItem_AddsTransformedItem()
     {
         // Arrange
         var source = new ObservableCollection<int>();
@@ -54,8 +52,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_InsertsTransformedItemsOnSourceInsert()
+    public void Insert_WhenSourceInsertsItem_InsertsTransformedItem()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -74,8 +71,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_RemovesTransformedItemsOnSourceRemove()
+    public void Remove_WhenSourceRemovesItem_RemovesTransformedItem()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -94,8 +90,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_UpdatesTransformedItemsOnSourceReplace()
+    public void Replace_WhenSourceReplacesItem_UpdatesTransformedItem()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -114,8 +109,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_MovesTransformedItemsOnSourceMove()
+    public void Move_WhenSourceMovesItem_MovesTransformedItem()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -135,8 +129,7 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Changes")]
-    public void DynamicObservableCollection_ResetsCollectionOnSourceReset()
+    public void Reset_WhenSourceResetsCollection_ResetsTransformedCollection()
     {
         // Arrange
         var source = new ObservableCollection<int>()
@@ -157,12 +150,13 @@ public class DynamicObservableCollectionTests
     }
 
     [TestMethod]
-    [TestCategory("DynamicObservableCollection - Disposal")]
-    public void DynamicObservableCollection_Dispose_UnregistersFromSource()
+    public void Dispose_WhenCalled_DisconnectsFromSource()
     {
         // Arrange
         var source = new ObservableCollection<int>();
         var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+        _ = collection; // Suppress warning about unused variable
+
         var registeredDelegates = EventHandlerTestHelper.FindAllRegisteredDelegates(
             source,
             "CollectionChanged");
@@ -176,5 +170,102 @@ public class DynamicObservableCollectionTests
             source,
             "CollectionChanged");
         _ = registeredDelegates.Should().NotContain(x => x.Method.Name == "OnSourceOnCollectionChanged");
+    }
+
+    [TestMethod]
+    public void Dispose_WhenCalledMultipleTimes_DoesNotThrow()
+    {
+        // Arrange
+        var source = new ObservableCollection<int>();
+        var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        collection.Dispose();
+        var act = collection.Dispose;
+
+        // Assert
+        _ = act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void AddMultiple_WhenSourceAddsMultipleItems_AddsTransformedItems()
+    {
+        // Arrange
+        var source = new ObservableCollection<int>();
+        using var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        source.Add(1);
+        source.Add(2);
+        source.Add(3);
+
+        // Assert
+        _ = collection.Count.Should().Be(3);
+        _ = collection.Should().Equal("Item 1", "Item 2", "Item 3");
+    }
+
+    [TestMethod]
+    public void RemoveMultiple_WhenSourceRemovesMultipleItems_RemovesTransformedItems()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2, 3 };
+        using var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        _ = source.Remove(1);
+        _ = source.Remove(2);
+
+        // Assert
+        _ = collection.Count.Should().Be(1);
+        _ = collection.Should().Equal("Item 3");
+    }
+
+    [TestMethod]
+    public void ReplaceMultiple_WhenSourceReplacesMultipleItems_UpdatesTransformedItems()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2, 3 };
+        using var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        source[0] = 4;
+        source[1] = 5;
+
+        // Assert
+        _ = collection.Count.Should().Be(3);
+        _ = collection.Should().Equal("Item 4", "Item 5", "Item 3");
+    }
+
+    [TestMethod]
+    public void MoveMultiple_WhenSourceMovesMultipleItems_MovesTransformedItems()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2, 3, 4 };
+        using var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        source.Move(0, 3); // Moves 1 to the end, resulting in { 2, 3, 4, 1 }
+        source.Move(1, 2); // Moves 3 one step to the right, resulting in { 2, 4, 3, 1 }
+
+        // Assert
+        _ = collection.Count.Should().Be(4);
+        _ = collection.Should().Equal("Item 2", "Item 4", "Item 3", "Item 1");
+    }
+
+    [TestMethod]
+    public void CombinationOperations_WhenSourcePerformsCombinationOperations_UpdatesTransformedCollection()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2, 3 };
+        using var collection = new DynamicObservableCollection<int, string>(source, this.transform);
+
+        // Act
+        source.Add(4);
+        _ = source.Remove(2);
+        source[0] = 5;
+
+        // Assert
+        _ = collection.Count.Should().Be(3);
+        _ = collection.Should().Equal("Item 5", "Item 3", "Item 4");
     }
 }
---
+++ projects/Collections/tests/ObservableCollectionExtensionsTests.cs
@@ -0,0 +1,202 @@
+// Distributed under the MIT License. See accompanying file LICENSE or copy
+// at https://opensource.org/licenses/MIT.
+// SPDX-License-Identifier: MIT
+
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+
+namespace DroidNet.Collections.Tests;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+[TestCategory("ObservableCollection Extensions")]
+public class ObservableCollectionExtensionsTests
+{
+    [TestMethod]
+    public void InsertInPlace_WhenCollectionIsSorted_InsertsItemInCorrectPosition()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int>
+        {
+            1,
+            3,
+            5,
+            7,
+        };
+        const int itemToAdd = 4;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Should().Equal(1, 3, 4, 5, 7);
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenCollectionIsEmpty_InsertsItem()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int>();
+        const int itemToAdd = 42;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Should().Equal(42);
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenItemHasDuplicateKey_InsertsItemNextToExistingKey()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int>
+        {
+            1,
+            3,
+            5,
+            7,
+        };
+        const int itemToAdd = 3; // Duplicate key
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Should().Equal(1, 3, 3, 5, 7); // Should insert next to existing 3
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenCollectionIsEmpty_AddsItem()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int>();
+        const int itemToAdd = 4;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Count.Should().Be(1);
+        _ = collection.Should().Equal(4);
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenCollectionIsNotEmpty_InsertsItemInCorrectPlace()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int> { 1, 3, 5, 7 };
+        const int itemToAdd = 4;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Count.Should().Be(5);
+        _ = collection.Should().Equal(1, 3, 4, 5, 7);
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenItemIsSmallest_InsertsItemAtStart()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int> { 2, 3, 4, 5 };
+        const int itemToAdd = 1;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Count.Should().Be(5);
+        _ = collection.Should().Equal(1, 2, 3, 4, 5);
+    }
+
+    [TestMethod]
+    public void InsertInPlace_WhenItemIsLargest_InsertsItemAtEnd()
+    {
+        // Arrange
+        var collection = new ObservableCollection<int> { 1, 2, 3, 4 };
+        const int itemToAdd = 5;
+
+        // Act
+        collection.InsertInPlace(itemToAdd, x => x, Comparer<int>.Default);
+
+        // Assert
+        _ = collection.Count.Should().Be(5);
+        _ = collection.Should().Equal(1, 2, 3, 4, 5);
+    }
+
+    [TestMethod]
+    public void Transform_WhenSourceHasItems_TransformsItems()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2, 3 };
+
+        // Act
+        using var result = source.Transform(x => $"Item {x * 10}");
+
+        // Assert
+        _ = result.Count.Should().Be(3);
+        _ = result.Should().Equal("Item 10", "Item 20", "Item 30");
+    }
+
+    [TestMethod]
+    public void Transform_WhenSourceAddsItem_AddsTransformedItem()
+    {
+        // Arrange
+        var source = new ObservableCollection<int>();
+
+        // Act
+        using var result = source.Transform(x => $"Item {x * 10}");
+        source.Add(4);
+
+        // Assert
+        _ = result.Count.Should().Be(1);
+        _ = result.Should().Equal("Item 40");
+    }
+
+    [TestMethod]
+    public void Transform_WhenSourceRemovesItem_RemovesTransformedItem()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2 };
+
+        // Act
+        using var result = source.Transform(x => $"Item {x * 10}");
+        _ = source.Remove(1);
+
+        // Assert
+        _ = result.Count.Should().Be(1);
+        _ = result.Should().Equal("Item 20");
+    }
+
+    [TestMethod]
+    public void Transform_WhenSourceReplacesItem_UpdatesTransformedItem()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2 };
+
+        // Act
+        using var result = source.Transform(x => $"Item {x * 10}");
+        source[0] = 3;
+
+        // Assert
+        _ = result.Count.Should().Be(2);
+        _ = result.Should().Equal("Item 30", "Item 20");
+    }
+
+    [TestMethod]
+    public void Transform_WhenSourceClearsCollection_ClearsTransformedCollection()
+    {
+        // Arrange
+        var source = new ObservableCollection<int> { 1, 2 };
+
+        // Act
+        using var result = source.Transform(x => $"Item {x * 10}");
+        source.Clear();
+
+        // Assert
+        _ = result.Count.Should().Be(0);
+    }
+}
