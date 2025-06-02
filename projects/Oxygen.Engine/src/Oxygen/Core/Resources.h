4cf2f287ebe9ee8451ac4f7690d483c61671c46c
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2025-06-02 08:46:50 +0400

    refactor: relete remanents of old design

--- projects/Oxygen.Engine/src/Oxygen/Core/Resources.h
+++ projects/Oxygen.Engine/src/Oxygen/Core/Resources.h
@@ -8,44 +8,21 @@
 
 #include <Oxygen/Base/ResourceHandle.h>
 
-#define DECLARE_RESOURCE(scope, Name)           \
-    using Name##Id = ResourceHandle;            \
-    namespace scope {                           \
-    struct Descriptor;                          \
-    }                                           \
-    using Name##Descriptor = scope::Descriptor; \
-    class Name;
+namespace oxygen::resources {
 
-namespace oxygen::world {
+// Reserved resource types for engine internal objects with IDs
+constexpr ResourceHandle::ResourceTypeT kReserved1 = 1;
+constexpr ResourceHandle::ResourceTypeT kReserved2 = 2;
+constexpr ResourceHandle::ResourceTypeT kReserved3 = 3;
+constexpr ResourceHandle::ResourceTypeT kReserved4 = 4;
+constexpr ResourceHandle::ResourceTypeT kReserved5 = 5;
+constexpr ResourceHandle::ResourceTypeT kReserved6 = 6;
+constexpr ResourceHandle::ResourceTypeT kReserved7 = 7;
+constexpr ResourceHandle::ResourceTypeT kReserved8 = 8;
+constexpr ResourceHandle::ResourceTypeT kReserved9 = 9;
+constexpr ResourceHandle::ResourceTypeT kReserved10 = 10;
 
-// ---------------------------------------------------------------------------
-// Resources:
-// ---------------------------------------------------------------------------
-// For each resource type, declare a ResourceTypeT constant and a resource,
-// using the `DECLARE_RESOURCE` macro. This will declare a specific name for
-// resource handle and add forward type declarations for the resource and its
-// descriptor.
-// ---------------------------------------------------------------------------
+// World resources
+constexpr ResourceHandle::ResourceTypeT kSceneNode = 13;
 
-namespace resources {
-    // Reserved resource types for engine internal objects with IDs
-    constexpr ResourceHandle::ResourceTypeT kReserved1 = 1;
-    constexpr ResourceHandle::ResourceTypeT kReserved2 = 2;
-    constexpr ResourceHandle::ResourceTypeT kReserved3 = 3;
-    constexpr ResourceHandle::ResourceTypeT kReserved4 = 4;
-    constexpr ResourceHandle::ResourceTypeT kReserved5 = 5;
-    constexpr ResourceHandle::ResourceTypeT kReserved6 = 6;
-    constexpr ResourceHandle::ResourceTypeT kReserved7 = 7;
-    constexpr ResourceHandle::ResourceTypeT kReserved8 = 8;
-    constexpr ResourceHandle::ResourceTypeT kReserved9 = 9;
-    constexpr ResourceHandle::ResourceTypeT kReserved10 = 10;
-
-    // World resources
-    constexpr ResourceHandle::ResourceTypeT kSceneNode = 13;
-} // namespace resources
-
-DECLARE_RESOURCE(scene, SceneNode)
-
-} // namespace oxygen::world
-
-#undef DECLARE_RESOURCE
+} // namespace oxygen::resources
