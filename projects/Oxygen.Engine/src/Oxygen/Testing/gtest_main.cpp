526914d6f095bcac262b6577a2f3732252a605b4
Abdessattar <457645+abdes@users.noreply.github.com>
2025-08-20 16:27:12 +0400

    style: re-format code for consistency

--- projects/Oxygen.Engine/src/Oxygen/Testing/gtest_main.cpp
+++ projects/Oxygen.Engine/src/Oxygen/Testing/gtest_main.cpp
@@ -10,44 +10,44 @@
 
 auto main(int argc, char** argv) -> int
 {
-    // Check the program was started just for test case discovery
-    bool list_tests = false;
-    for (int i = 1; i < argc; ++i) {
-        if (std::strcmp(argv[i], "--gtest_list_tests") == 0) {
-            list_tests = true;
-            break;
-        }
+  // Check the program was started just for test case discovery
+  bool list_tests = false;
+  for (int i = 1; i < argc; ++i) {
+    if (std::strcmp(argv[i], "--gtest_list_tests") == 0) {
+      list_tests = true;
+      break;
     }
+  }
 
-    if (!list_tests) {
+  if (!list_tests) {
 #if defined(_MSC_VER)
-        // Enable memory leak detection in debug mode
-        _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    // Enable memory leak detection in debug mode
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-        loguru::g_preamble_date = false;
-        loguru::g_preamble_file = true;
-        loguru::g_preamble_verbose = false;
-        loguru::g_preamble_time = false;
-        loguru::g_preamble_uptime = false;
-        loguru::g_preamble_thread = false;
-        loguru::g_preamble_header = false;
-        loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
-        loguru::init(argc, argv);
+    loguru::g_preamble_date = false;
+    loguru::g_preamble_file = true;
+    loguru::g_preamble_verbose = false;
+    loguru::g_preamble_time = false;
+    loguru::g_preamble_uptime = false;
+    loguru::g_preamble_thread = false;
+    loguru::g_preamble_header = false;
+    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
+    loguru::init(argc, argv);
 #if !defined(NDEBUG)
-        loguru::g_stderr_verbosity = loguru::Verbosity_3;
+    loguru::g_stderr_verbosity = loguru::Verbosity_3;
 #else
-        loguru::g_stderr_verbosity = loguru::Verbosity_0;
+    loguru::g_stderr_verbosity = loguru::Verbosity_0;
 #endif // !NDEBUG
-    }
+  }
 
-    testing::InitGoogleTest(&argc, argv);
-    const auto ret = RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  const auto ret = RUN_ALL_TESTS();
 
-    if (!list_tests) {
-        loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
-        loguru::shutdown();
-    }
+  if (!list_tests) {
+    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
+    loguru::shutdown();
+  }
 
-    return ret;
+  return ret;
 }
