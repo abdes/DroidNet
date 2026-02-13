81564576a774bbbf63377744b0b8b489488592c3
Abdessattar <457645+abdes@users.noreply.github.com>
2026-02-13 21:46:08 +0400

    feat(icons): add icon for grid plan panel

--- projects/Oxygen.Engine/src/Oxygen/ImGui/Icons/IconsOxygenIcons.h
+++ projects/Oxygen.Engine/src/Oxygen/ImGui/Icons/IconsOxygenIcons.h
@@ -6,7 +6,6 @@
 
 #pragma once
 
-#include <cstdint>
 #include <string_view>
 
 // Auto-generated icon constants for OxygenIcons.
@@ -21,17 +20,19 @@ inline constexpr char32_t kIconDemoPanelCodepoint = 0xE002;
 inline constexpr std::string_view kIconDemoPanel = "\xEE\x80\x82";
 inline constexpr char32_t kIconEnvironmentCodepoint = 0xE003;
 inline constexpr std::string_view kIconEnvironment = "\xEE\x80\x83";
-inline constexpr char32_t kIconHdrTonemapCodepoint = 0xE004;
-inline constexpr std::string_view kIconHdrTonemap = "\xEE\x80\x84";
-inline constexpr char32_t kIconLightingCodepoint = 0xE005;
-inline constexpr std::string_view kIconLighting = "\xEE\x80\x85";
-inline constexpr char32_t kIconRenderSolidCodepoint = 0xE006;
-inline constexpr std::string_view kIconRenderSolid = "\xEE\x80\x86";
-inline constexpr char32_t kIconRenderWireframeCodepoint = 0xE007;
-inline constexpr std::string_view kIconRenderWireframe = "\xEE\x80\x87";
-inline constexpr char32_t kIconRenderingCodepoint = 0xE008;
-inline constexpr std::string_view kIconRendering = "\xEE\x80\x88";
-inline constexpr char32_t kIconSettingsCodepoint = 0xE009;
-inline constexpr std::string_view kIconSettings = "\xEE\x80\x89";
+inline constexpr char32_t kIconGrid2x2Codepoint = 0xE004;
+inline constexpr std::string_view kIconGrid2x2 = "\xEE\x80\x84";
+inline constexpr char32_t kIconHdrTonemapCodepoint = 0xE005;
+inline constexpr std::string_view kIconHdrTonemap = "\xEE\x80\x85";
+inline constexpr char32_t kIconLightingCodepoint = 0xE006;
+inline constexpr std::string_view kIconLighting = "\xEE\x80\x86";
+inline constexpr char32_t kIconRenderSolidCodepoint = 0xE007;
+inline constexpr std::string_view kIconRenderSolid = "\xEE\x80\x87";
+inline constexpr char32_t kIconRenderWireframeCodepoint = 0xE008;
+inline constexpr std::string_view kIconRenderWireframe = "\xEE\x80\x88";
+inline constexpr char32_t kIconRenderingCodepoint = 0xE009;
+inline constexpr std::string_view kIconRendering = "\xEE\x80\x89";
+inline constexpr char32_t kIconSettingsCodepoint = 0xE00A;
+inline constexpr std::string_view kIconSettings = "\xEE\x80\x8A";
 
 } // namespace oxygen::imgui::icons
